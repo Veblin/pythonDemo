f6b9b05d2cdf61531badf0aa65e62e79d918ca1f
Wubin <wubin@yiche.com>
2017-03-21 18:01:49 +0800

    提交代码

--- shibor/getData.py
+++ shibor/getData.py
@@ -1,12 +1,26 @@
 import requests
+import sys
+from datetime import datetime
+from urllib.request import urlretrieve
 
+# lib for excel & mongo
+import xlrd
+import sys
+import json
+import pymongo
+from pymongo import MongoClient
 
 # get Data from URL 
 # DEMO 
-
+#Shibor URL
 # http://www.shibor.org/shibor/web/html/downLoad.html?nameNew=Historical_Shibor_Data_2016.xls&nameOld=Shibor%CA%FD%BE%DD2016.xls&shiborSrc=http%3A%2F%2Fwww.shibor.org%2Fshibor%2F&downLoadPath=data
 
-# http://www.shibor.org/shibor/web/html/downLoad.html?nameNew=Historical_Shibor_Data_2017.xls&nameOld=Shibor%CA%FD%BE%DD2017.xls&shiborSrc=http%3A%2F%2Fwww.shibor.org%2Fshibor%2F&downLoadPath=data
+#Report URL
+#http://www.shibor.org/shibor/web/html/downLoad.html?nameNew=Historical_Quote_Data_2016.xls&nameOld=%B1%A8%BC%DB%CA%FD%BE%DD2016.xls&shiborSrc=http%3A%2F%2Fwww.shibor.org%2Fshibor%2F&downLoadPath=data
+
+#Shibor Ava URL
+#http://www.shibor.org/shibor/web/html/downLoad.html?nameNew=Historical_Shibor_Tendency_Data_2016.xls&nameOld=Shibor%BE%F9%D6%B5%CA%FD%BE%DD2016.xls&shiborSrc=http%3A%2F%2Fwww.shibor.org%2Fshibor%2F&downLoadPath=data
+
 
 # Headers 
 # GET /shibor/web/html/downLoad.html?nameNew=Historical_Shibor_Data_2017.xls&nameOld=Shibor%CA%FD%BE%DD2017.xls&shiborSrc=http%3A%2F%2Fwww.shibor.org%2Fshibor%2F&downLoadPath=data HTTP/1.1
@@ -22,17 +36,54 @@ import requests
 # Accept-Language: en-US,en;q=0.8,zh-CN;q=0.6
 # Cookie: JSESSIONID=2hl2YPjMn1H4mPB3sQDyzbVQTWVDTwYLY53xQTh2Q9QYSpfNhzrK!-957207357; _gscu_67677853=899794585tquw176; _gscs_67677853=t89986570ltvzan15|pv:1; _gscbrs_67677853=1
 
-def makeUrl(date):
+fileDic = {
+    'shibor':('Historical_Shibor_Data_','Shibor%CA%FD%BE%DD'),
+    'report':('Historical_Quote_Data_','%B1%A8%BC%DB%CA%FD%BE%DD'),
+    'shiborAva':('Historical_Shibor_Tendency_Data_','Shibor%BE%F9%D6%B5%CA%FD%BE%DD')
+}
+filePath = sys.path[0] +'/files/'
+
+def getFile(year,type):
     # date's format like Year YYYY
-    _url = 'http://www.shibor.org/shibor/web/html/downLoad.html?nameNew=Historical_Shibor_Data_'+date+'.xls&nameOld=Shibor%CA%FD%BE%DD'+date+'.xls&shiborSrc=http%3A%2F%2Fwww.shibor.org%2Fshibor%2F&downLoadPath=data'
+    _date = str(year)
+    
+    _url = 'http://www.shibor.org/shibor/web/html/downLoad.html?nameNew='+ fileDic[type][0] +_date+'.xls&nameOld='+fileDic[type][1]+_date+'.xls&shiborSrc=http%3A%2F%2Fwww.shibor.org%2Fshibor%2F&downLoadPath=data'
 
     _headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
         'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
         'Accept-Encoding': 'gzip, deflate, sdch'
     }
+    _fileName = filePath + fileDic[type][0] + _date + '.xls'
+    print ('Download file to' + _fileName)
+    return urlretrieve (_url, _fileName)
+    # _req = requests.get(_url,headers = _headers)
+    # if (_req.status_code == 200) : 
+    #     return _req.content
 
-    return requests.get(_url,headers = _headers)
+def getFileList():
+    _startYear = 2006
+    _thisYear = datetime.now().year
+    _typeList = ('shibor','report','shiborAva')
+    # TODO 完善循环体
+    for type in _typeList:
+        __nowYear = _startYear
+        while __nowYear <= _thisYear:
+            getFile(__nowYear,type)
+            __nowYear += + 1
 
+def openXlsFile (filename):
+    data = xlrd.open_workbook(filename+'.xls')
 
-print(makeUrl('2017'))
+def toDB ():
+    _client = MongoClient('localhost',27017)
+    _db = _client.scrapy
+    _data = xlrd.open_workbook(filePath + 'Historical_Quote_Data_2006.xls')
+    _table = data.sheets()[0]
+    _th = _table.row_values(0)
+    _tr = _table.nrows
+    print(_th)
+
+if __name__ == "__main__":
+    # getFileList()
+    toDB()
