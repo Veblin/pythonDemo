95f0bd597e0f91fb0393b6b383187f8894df591a
Wubin <wubin@yiche.com>
2017-03-29 17:54:24 +0800

    提交代码

--- shibor/getDateFromDB.py
+++ shibor/getDateFromDB.py
@@ -4,11 +4,15 @@ import sys
 import json
 import pymongo
 from pymongo import MongoClient
+import flask 
+from flask import request,Flask
 
 _conditions = {
     'start':'2016-01-01',
     'end':'2016-04-01'
 }
+
+app = Flask(__name__)
 
 def getData(
     tableName = 'shibor',
@@ -27,8 +31,9 @@ def getData(
         # 过滤数据
         __data =  _table.find_one({'Date':__date})
         # 把符合过滤条件的数据添加到数组
-        _arr.append(__data)
-        print(__data)
+        if __data != None:
+            _arr.append(__data)
+        # print(__data)
 
     return _arr
     
@@ -39,5 +44,57 @@ def formatDateTime (str):
     finally:
         return
 
+def apiRetData(data):
+    api = []
+    yKey = ['O/N','1W','2W','3M','6M','9M','1Y']
+    for i in range(0,len(data)):
+        _data = data[i]
+        _yData = fillData(_data,yKey)
+        _api = {
+            'x':[
+                {
+                    'data':_data['Date']
+                }
+            ],
+            'y':[
+                _yData
+            ]
+        }
+        api.append(_api)
+
+    return api
+
+def fillData(data,arr):
+    res = {}
+    for i in range(0,len(arr)):
+        _key = arr[i]
+        res[_key] = data[_key]
+
+    return res
+
+
+
+
+@app.route('/api/shibor',methods = ['POST'])
+def apiShibor():
+    # show the post with the given id, the id is an integer
+    start = request.values['start_time']
+    end =  request.values['end_time']
+    if start is None or end is None:
+        abort(400)
+    
+    
+    _conditions={
+        'start':start,
+        'end':end
+    }
+    # TODO 完善 API接口
+    print(apiRetData(getData('shibor',_conditions)))
+        # return 'Post %d' % post_id
+
+
+
 if __name__ == "__main__":
-    getData('shibor',_conditions)
+    # 返回数据
+    app.run()
+    
